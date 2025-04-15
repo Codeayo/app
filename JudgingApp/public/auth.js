32f4a765579e8da47fbe1f3e9ac6ab94e7e2a137
Codeayo <105753515+Codeayo@users.noreply.github.com>
2025-04-15 17:52:08 -0400

    Update auth.js

--- JudgingApp/public/auth.js
+++ JudgingApp/public/auth.js
@@ -9,7 +9,7 @@ if (judgeLoginForm) {
     const password = document.getElementById("password").value;
 
     try {
-      const res = await fetch("http://localhost:3000/api/judges/login", {
+      const res = await fetch("https://judgingapp-97rm.onrender.com/api/judges/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ judgeId, password }),
@@ -46,7 +46,7 @@ if (judgeRegisterForm) {
     }
 
     try {
-      const res = await fetch("http://localhost:3000/api/judges/register", {
+      const res = await fetch("https://judgingapp-97rm.onrender.com/api/judges/register", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ judgeId, password }),
@@ -77,7 +77,7 @@ if (studentLoginForm) {
     const password = document.getElementById("password").value;
 
     try {
-      const res = await fetch("http://localhost:3000/api/student/login", {
+      const res = await fetch("https://judgingapp-97rm.onrender.com/api/student/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, password }),
@@ -116,7 +116,7 @@ if (studentRegisterForm) {
     }
 
     try {
-      const res = await fetch("http://localhost:3000/api/student/register", {
+      const res = await fetch("https://judgingapp-97rm.onrender.com/api/student/register", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ name, email, password }),
